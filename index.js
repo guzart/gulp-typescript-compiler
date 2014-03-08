80560227e72a651bc23a1cc7f1b08550a3f00940
Arturo Guzman <arturo@kisurfer.com>
2014-03-08 00:51:24 -0500

    add basic working functionality for sourcemaps

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 /* jshint latedef: false */
 'use strict';
 
-// var path = require('path');
-// var es = require('event-stream');
-
 var gutil = require('gulp-util');
 var through = require('through2');
 var ts = require('typescript-api');
 
-
 module.exports = tsPugin;
-
 
 function tsPugin(options) {
 
@@ -21,6 +16,7 @@ function tsPugin(options) {
 
     function objectStream(file, enc, cb) {
     /* jshint validthis: true */
+        var sourcemap;
 
         if (file.isNull()) {
             this.push(file);
@@ -36,15 +32,32 @@ function tsPugin(options) {
             var result = compile(file, settings);
             file.contents = new Buffer(result.contents);
             file.path = gutil.replaceExtension(file.path, '.js');
+            if (options.sourcemap) {
+                sourcemap = buildSourcemapFile(file, result.sourcemap);
+            }
         } catch (err) {
             err.fileName = file.path;
             this.emit('error', error(err));
         }
 
         this.push(file);
+        if (sourcemap) {
+            this.push(sourcemap);
+        }
+
         cb();
     }
 }
+
+function buildSourcemapFile(source, content) {
+    return new gutil.File({
+        base: source.base,
+        cwd: source.cwd,
+        path: gutil.replaceExtension(source.path, '.js.map'),
+        contents: new Buffer(content)
+    });
+}
+
 
 function error(msg) {
     return new gutil.PluginError('gulp-typescript', msg);
@@ -67,12 +80,21 @@ function compile(file, settings) {
     var snapshot = ts.ScriptSnapshot.fromString(file.contents.toString());
     compiler.addFile(file.path, snapshot);
 
-    var iter = compiler.compile();
+    var it = compiler.compile();
 
     var output = '';
-    while (iter.moveNext()) {
-        var current = iter.current().outputFiles[0];
-        output += !!current ? current.text : '';
+    var sourcemap = '';
+    while (it.moveNext()) {
+        var result = it.current();
+        for (var i = 0; i < result.outputFiles.length; i++) {
+            var current = result.outputFiles[i];
+            if (!current) { continue; }
+            if (/\.map$/.test(current.name)) {
+                sourcemap += current.text;
+            } else {
+                output += current.text;
+            }
+        }
     }
 
     var diagnostics = compiler.getSemanticDiagnostics(file.path);
@@ -80,5 +102,5 @@ function compile(file, settings) {
         error(diagnostics[0].text());
     }
 
-    return { contents: output };
+    return { contents: output, sourcemap: sourcemap };
 }
