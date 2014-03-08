b477435a1621bf001808600a207fd827fec0be0c
Arturo Guzman <arturo@kisurfer.com>
2014-03-07 22:21:35 -0500

    finish merging both plugins into one

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path');
+var Buffer = require('buffer').Buffer;
 var gutil = require('gulp-util');
 var through = require('through2');
 var ts = require('typescript-api');
@@ -9,13 +10,35 @@ var ts = require('typescript-api');
 // var es = require('event-stream');
 // var Buffer = require('buffer').Buffer;
 
-
-var filename = 'typestring.ts';
-
-
-
-
 module.exports = function (opt) {
+    var filename = 'typestring.ts';
+
+    function compile(input) {
+        // var logger = ;
+        // var settings = ;
+        var compiler = new ts.TypeScriptCompiler();
+        
+        var snapshot = ts.ScriptSnapshot.fromString(input);
+        compiler.addFile(filename, snapshot);
+
+        var iter = compiler.compile();
+
+        var output = '';
+        while (iter.moveNext()) {
+            var current = iter.current().outputFiles[0];
+            output += !!current ? current.text : '';
+        }
+
+        var diagnostics = compiler.getSemanticDiagnostics(filename);
+        if (!output && diagnostics.length) {
+            throw new Error(diagnostics[0].text());
+        }
+        gutil.log('input: ' + input);
+        gutil.log('output: ' + output);
+        return output;
+    }
+
+
     return through.obj(function (file, enc, cb) {
         if (file.isNull()) {
             this.push(file);
@@ -38,9 +61,4 @@ module.exports = function (opt) {
         this.push(file);
         cb();
     });
-
-    function compile(input) {
-        // var compiler = new TypeScript.TypeScriptCompiler(new );
-        return input;
-    }
 };
